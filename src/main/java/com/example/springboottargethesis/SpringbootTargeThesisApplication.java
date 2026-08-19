dcd6ef2f0413aa9dde7fe17c2aa2f6c504cf7f47
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2026-08-19 10:28:19 +0300

    INIT: first init

---
+++ src/main/java/com/example/springboottargethesis/SpringbootTargeThesisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottargethesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTargeThesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTargeThesisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottargethesis/SpringbootTargeThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottargethesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTargeThesisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
